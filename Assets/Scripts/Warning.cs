c42defc28febc7e2e8e20beaaf3ca9ca3e94426e
Alex Calixte <alexcalixte1994@gmail.com>
2020-10-27 15:14:34 -0400

    Warning
    Warning sign to remind users of surroundings

---
+++ Assets/Scripts/Warning.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Warning : MonoBehaviour
+{
+    public GameObject warning;
+    public GameObject screenspaceUI;
+    // Start is called before the first frame update
+
+
+    public void WarningDialog()
+    {
+        warning.SetActive(false);
+        screenspaceUI.SetActive(true);
+
+    }
+}
